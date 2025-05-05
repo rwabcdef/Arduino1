af3c91e4a2ba254381ea0e82f6933fdd3d09b6cb
rwabcdef <rwabcdef1234@gmail.com>
2025-05-05 21:54:19 +0100

    Added debug to  SerLink

--- PC/Transport/Python/SerLink.py
+++ PC/Transport/Python/SerLink.py
@@ -1,11 +1,12 @@
 import serial
-import traceback, threading
+import traceback, time, threading, datetime 
 
 #---------------------------------------------------
 class SerLink:
   def __init__(self, port, baudrate):
+    self.debug = SerLink.Debug()
     self.port = SerLink.Port(port, baudrate)
-    self.reader = SerLink.Reader(self.port)
+    self.reader = SerLink.Reader(self.port, self.debug)
 
   def start(self):
     self.port.init()
@@ -13,7 +14,7 @@ class SerLink:
     self.readerThread.start()
 
   def close(self):
-    self.reader.setQuit()
+    self.reader.quit()
     self.readerThread.join()
     self.port.close()
 
@@ -72,9 +73,13 @@ class SerLink:
       s = '%s, %s, %s, %s, %s' % (self.protocol, self.type, self.rollCode, self.dataLen, self.data)
       print(s)
 
+    def sprint(self):
+      s = '%s, %s, %s, %s, %s' % (self.protocol, self.type, self.rollCode, self.dataLen, self.data)
+      return s
+
   class Port:
-    def __init__(self, port, baudrate):
-      self.port = serial.Serial(port, baudrate, timeout=None)
+    def __init__(self, port, baudrate, timeout=1):
+      self.port = serial.Serial(port, baudrate, timeout=timeout)
       
     def init(self):
       if self.port.isOpen():
@@ -92,28 +97,49 @@ class SerLink:
       return
     
     def readline(self):
-      return self.port.readline()
+      line = self.port.readline()
+      if len(line) == 0:
+        return ''
+      else:
+        return line.decode("utf-8")
     
     def write(self, data):
-      self.port.write(data)
+      self.port.write(data.encode('utf-8'))
     
     def close(self):
       self.port.close()
     
   class Reader:
-    def __init__(self, port):
+    def __init__(self, port, debug=None):
       self.port = port
-      self.quit = False
+      self.quitFlag = False
       self.rxFrame = SerLink.Frame()
+      self.debug = debug
       #self.ackFrame = SerLink.Frame()
 
+    def print(self, s):
+      if(self.debug != None):
+        self.debug.printer.print(s)
+      else:
+        print(s)
+
     def run(self):
-      while(self.quit == False):
-        line = self.port.readline().decode("utf-8")
-        print('rx[%d]> %s' % (len(line), line))
+      if(self.debug != None):
+        self.debug.threadNameResolver.addCurrentThread('RDR  ')
+
+      while(self.quitFlag == False):
+        line = self.port.readline()
+        if len(line) == 0:
+          continue
+
+        #s = 'rx[%d]> %s' % (len(line), line)
+        
+
+        self.print('rx[%d]> %s' % (len(line), line))
+
         if(len(line) >= SerLink.Frame.LEN_HEADER + 1):
           self.rxFrame.fromString(line)
-          self.rxFrame.print()
+          self.print(self.rxFrame.sprint())
 
           if(self.rxFrame.type == SerLink.Frame.TYPE_TRANSMISSION):
             # send ack
@@ -121,10 +147,60 @@ class SerLink:
 
             # ack send
             ackFrameStr = ackFrame.toString()
-            self.port.write(ackFrameStr.encode('utf-8'))
+            self.port.write(ackFrameStr)
 
-    def setQuit(self):
-      self.quit = True
+    def quit(self):
+      self.quitFlag = True
+
+  class Debug:
+    def __init__(self):
+      self.threadNameResolver = SerLink.Debug.ThreadNameResolver()
+      self.timeStamper = SerLink.Debug.TimeStamper()
+      self.printer = SerLink.Debug.Printer(self.threadNameResolver, self.timeStamper)
+
+    class ThreadNameResolver:
+      def __init__(self):
+        self.dict = dict()
+
+      def addThreadName(self, threadId, threadName):
+        self.dict[threadId] = threadName
+
+      def addCurrentThread(self, threadName):
+        threadId = threading.current_thread().ident
+        self.dict[threadId] = threadName
+
+      def getCurrentThreadName(self):
+        if(threading.current_thread().ident in self.dict):
+          return self.dict[threading.current_thread().ident]
+        else:
+          return '-----'
+        
+    class TimeStamper(object):
+      def __init__(self):
+        self.start = datetime.datetime.now()
+      
+      """
+      Returns string containing elapsed time: seconds:milliseconds
+      e.g.: 015.745
+      """
+      def getElapsed(self):
+        elapsed = datetime.datetime.now() - self.start
+        t1 = int(elapsed.microseconds / 1000)
+        seconds = str(elapsed.seconds)
+        milliseconds = str(t1).zfill(3)
+        seconds = str(elapsed.seconds).zfill(3)
+
+        s = '{seconds:s}.{milliseconds:s}'.format(seconds = seconds, milliseconds = milliseconds)
+        #s = '{seconds:2d}.{milliseconds:3d}'.format(seconds = 2, milliseconds = 32)
+        return s
+  
+    class Printer:
+      def __init__(self, threadNameResolver, timeStamper):
+        self.threadNameResolver = threadNameResolver
+        self.timeStamper = timeStamper
+
+      def print(self, s):
+        print('[%s:%s] %s' % (self.threadNameResolver.getCurrentThreadName(), self.timeStamper.getElapsed(), s))
 
 #---------------------------------------------------
 # Run:
